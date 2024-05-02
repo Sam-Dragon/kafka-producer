b43b425d93879f7d5f640772583f37c29f8b2967
Sam-Dragon <rahulsheth2013@gmail.com>
2024-05-02 23:59:57 +0530

    kafka-producer-config: - initial commit

---
+++ src/main/java/com/kafka/producer/KafkaProducerApplication.java
@@ -0,0 +1,21 @@
+package com.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+    @GetMapping
+    public String loading() {
+        return "Producer is up and running";
+    }
+
+}
